1f5ac700ca7fa52f40e32b63028c1ff9b52cbed2
Matthias <campe@berlinale.de>
2023-04-12 17:41:51 +0200

    remove old import

--- src/components/RecipeItem.tsx
+++ src/components/RecipeItem.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { makeStyles, createStyles } from '@material-ui/styles';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
